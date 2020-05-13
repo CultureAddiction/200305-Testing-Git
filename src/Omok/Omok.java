e26bd074759433291d837343bcb3b3e889479dd0
didqh <didqh@DESKTOP-85HA6GL>
2020-05-14 01:24:27 +0900

    // 01. 주석, validMove 정상화(후위연산자 사용), moveResult 주시중 -checkBugOn 상수 도입

---
+++ src/Omok/Omok.java
@@ -0,0 +1,330 @@
+package Omok;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+/**
+ * <code>오목 게임</code> - 일반룰(오목룰)을 이용하는 오목판 및 게임.
+ * https://namu.wiki/w/%EC%98%A4%EB%AA%A9(%EA%B2%8C%EC%9E%84)
+ *
+ * 사용법: 커맨드라인에서 java Omok [<판 크기>]
+ * @author 꿀쥐
+ * @version 1.0
+ */
+public class Omok
+{
+    /**
+     * <code>메인</code> - 오목판을 초기화
+     * 판 크기는 기본적으로 15지만 커맨드라인에서 실행할 때 설정할 수 있다.
+     *
+     * @param args a <code>String[]</code> value - command line
+     * arguments
+     */
+    public static void main(String[] args) {
+
+	int size = 15;
+	if (args.length > 0)
+	    size = Integer.parseInt(args[0]);
+
+	JFrame frame = new JFrame();
+	
+	final int FRAME_WIDTH = 600;
+	final int FRAME_HEIGHT = 650;
+	frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+	frame.setTitle("Omok");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	OmokPanel panel = new OmokPanel(size);
+	frame.add(panel);
+	
+	frame.setVisible(true);
+    
+    }
+}
+
+class OmokState {
+	public static final int NONE = 0;
+	public static final int BLACK = 1;
+	public static final int WHITE = 2;
+	private int size;
+	private int winner;
+	private int currentPlayer;
+	private int board[][];
+	private boolean isSwitchOK = true;
+	public OmokState(int size) {
+		this.size = size;
+		board = new int[size][size];
+		currentPlayer = BLACK;
+	}
+	
+	public void playPiece(int row, int col) {
+
+		System.out.println("Try Place at row,column " + row +","+ col+" as Player"+currentPlayer);
+		if (validMove(row, col))
+			board[row][col] = currentPlayer;
+		else{// 여기에 둘수 없다고 명령이 뜨면 -> 변수 하나를 추가 해서 currentPlayer - Switch가 false가 되도록
+			JOptionPane.showMessageDialog(null, "여기에 둘 수 없습니다.");
+			isSwitchOK = false;
+		}
+		switch (currentPlayer) {	
+		
+		case BLACK:
+			if (isSwitchOK)
+			currentPlayer = WHITE;		// 다음 플레이를 결정하는 명령.
+			isSwitchOK= true;
+			break;
+		case WHITE:
+			if (isSwitchOK)
+			currentPlayer = BLACK;
+			isSwitchOK=true;
+			break;
+		}
+	}
+	
+	public int getPiece(int row, int col) {
+		return board[row][col];
+	}
+	
+	public int getWinner() {
+		return winner;
+	}
+	
+	public boolean validMove(int row, int col) {
+		// 	validMove가 	false면 여기에 둘 수 없다는 message를 출력한다.
+		// 				true면 진행		
+		int r = row, c = col;
+		/*
+		 * step
+		 * 수직: 0(북), 1(남)
+		 * 수평: 2(동), 3(서)
+		 * 사선: 4(동북), 5(서남), 6(서북), 7(동남)
+		 */
+		int step = 0;
+		int[] stepCount = new int[8];	// 오목이 성립하는 모든 조건을(8가지) 검사하는 배열
+		boolean doneCheck = false;
+		while (!doneCheck) {
+
+			switch (step) {
+			// if문에서는 step이 살펴볼 방향을 지정하며, r과 c를 수정하면서 순차적으로 살펴보며 놓여진 돌의 갯수를 stepCount의 결과를 낸다.
+			// else문에서는 step을 다음 단계로 지정하며, r와 c를 초기 row값으로 되돌려 놓는다.
+			// 예시. 흑돌이 놓은 자리 위에 흑돌이 셋, 아래에 흑돌이 하나 있으면 case0는 탐색 세번 = 초기화 한번, case1은 탐색 1번 초기화 
+			//													되어야 하는데.... if문 아래로 전혀 들어가지 않는다.
+			case 0:
+				if (!outOfBounds(r-1) && sameColor(--r, c))	//여긴 왜 r--? 북쪽을 쭉 갈거기 때문에 --- case 0은 여러번 호출된다.
+					stepCount[step]++;						// if문 안으로 가질 않기 때문에 stepCount가 증가 되지 않는다 - 조건문에 문제 확인.
+				else { step++; r = row; c = col; }			// 문제 해결: 두 조건문 함수에는 이상X. 인수를 살펴봄 --- 이상 발견
+				break;
+			case 1:
+				if (!outOfBounds(r+1) && sameColor(++r, c))
+					stepCount[step]++;
+				else { step++; r = row; c = col; }
+				break;
+			case 2:
+				if (!outOfBounds(c+1) && sameColor(r, ++c))
+					stepCount[step]++;
+				else { step++; r = row; c = col; }
+				break;
+			case 3:
+				if (!outOfBounds(c-1) && sameColor(r, --c))
+					stepCount[step]++;
+				else { step++; r = row; c = col; }
+				break;
+			case 4:
+				if (!outOfBounds(r-1) && !outOfBounds(c+1) && sameColor(--r, ++c))
+					stepCount[step]++;
+				else { step++; r = row; c = col; }
+				break;
+			case 5:
+				if (!outOfBounds(r+1) && !outOfBounds(c-1) && sameColor(++r, --c))
+					stepCount[step]++;
+				else { step++; r = row; c = col; }
+				break;
+			case 6:
+				if (!outOfBounds(r-1) && !outOfBounds(c-1) && sameColor(--r, --c))
+					stepCount[step]++;
+				else { step++; r = row; c = col; }
+				break;
+			case 7:
+				if (!outOfBounds(r+1) && !outOfBounds(c+1) && sameColor(++r, ++c))
+					stepCount[step]++;
+				else { step++; r = row; c = col; }
+				break;
+			default:
+				doneCheck = true;
+				break;
+			}
+		}
+		// moveResult는 승자를 결정하면 0을 return, 결정되지 않았다면 1과 2를 return
+		// 1과 2는? 2는 육목일 경우. 1은 ???
+		int result = moveResult(stepCount);
+		
+		if (result == 0) winner = currentPlayer;
+		
+		if (result == 1 || result == 2)
+			return false;
+		
+		return true;
+	}
+	
+	// switch case문에서 stepCount[]를 더해주기 위한 조건 함수 2개
+	public boolean outOfBounds(int n) {
+		// 함수 내부 이상 무
+		return !(n >= 0 && n < size);
+	}
+	
+	public boolean sameColor(int r, int c) {
+		// 함수 내부 이상 무
+		return board[r][c] == currentPlayer;
+	}
+	
+	
+	/*
+	 * 이기는 수(5): 0
+	 * 금수(33 혹은 44): 1
+	 * 장목(6이상): 2
+	 * 수: 3
+	 */
+	public int moveResult(int[] stepCount) {	// return값이 1,2면 false, 0이면 승자 결정.
+		final int checkBugOn33 = 0;
+		final int checkBugOn44 = 1;
+		int countTwo = 0, countThree = 0;
+		boolean win = false;
+		for (int i=0; i<8; i++) {
+			// 1. moveResult 2 혹은 0을 결정하는 if-else
+			if (i % 2 == 1 && (stepCount[i-1] + stepCount[i] > 5-1)) 
+								// sc[0] + sc[1] > 5, sc[3] + sc[4] > 6 .... 
+								// -> 북+남이 6,7,8,... 동+서가 6,7,8... 이런경우 = 6목 일때 
+				return 2;		// 6목, 7목 
+			else 
+				if (i % 2 == 1 && (stepCount[i-1] + stepCount[i] == 5-1))
+								// 북 + 남 = 5, 동 + 서 = 5, 
+					win = true;
+			
+			// moveresult 1을 결정하는 if-else
+			if (stepCount[i] == 2-1) 
+				countTwo++;
+			else
+				if (stepCount[i] == 3-1) 
+					countThree++;
+		}
+		
+		// 아래는 return값을 결정하는 if문들
+		if (countTwo >= 2-checkBugOn33 || countThree >= 2-checkBugOn44)
+			return 1;
+		if (win)
+			return 0;
+		return 3;
+	}
+	
+	
+	
+	
+}
+
+class OmokPanel extends JPanel
+{
+    private final int MARGIN = 5;
+    private final double PIECE_FRAC = 0.9;
+
+    private int size = 19;
+    private OmokState state;
+    
+    public OmokPanel() 
+    {
+	this(15);
+    }
+
+    public OmokPanel(int size) 
+    {
+	super();
+	this.size = size;
+	state = new OmokState(size);
+	addMouseListener(new GomokuListener());
+    }
+
+    class GomokuListener extends MouseAdapter 
+    {
+	public void mouseReleased(MouseEvent e) 
+	{
+	    double panelWidth = getWidth();
+	    double panelHeight = getHeight();
+	    double boardWidth = Math.min(panelWidth, panelHeight) - 2 * MARGIN;
+	    double squareWidth = boardWidth / size;
+	    double pieceDiameter = PIECE_FRAC * squareWidth;
+	    double xLeft = (panelWidth - boardWidth) / 2 + MARGIN;
+	    double yTop = (panelHeight - boardWidth) / 2 + MARGIN;
+	    int col = (int) Math.round((e.getX() - xLeft) / squareWidth - 0.5);
+	    int row = (int) Math.round((e.getY() - yTop) / squareWidth - 0.5);
+	    if (row >= 0 && row < size && col >= 0 && col < size
+		&& state.getPiece(row, col) == OmokState.NONE
+		&& state.getWinner() == OmokState.NONE) {
+		state.playPiece(row, col);
+		repaint();
+		int winner = state.getWinner();
+		if (winner != OmokState.NONE)
+		    JOptionPane.showMessageDialog(null,
+                      (winner == OmokState.BLACK) ? "Black wins!" 
+						    : "White wins!");
+	    }
+	}    
+    }
+    
+    
+    public void paintComponent(Graphics g) 
+    {
+	Graphics2D g2 = (Graphics2D) g;
+	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+			    RenderingHints.VALUE_ANTIALIAS_ON);
+	
+	double panelWidth = getWidth();
+	double panelHeight = getHeight();
+
+	g2.setColor(new Color(0.925f, 0.670f, 0.34f)); // 나무색
+	g2.fill(new Rectangle2D.Double(0, 0, panelWidth, panelHeight));
+
+	
+	double boardWidth = Math.min(panelWidth, panelHeight) - 2 * MARGIN;
+	double squareWidth = boardWidth / size;
+	double gridWidth = (size - 1) * squareWidth;
+	double pieceDiameter = PIECE_FRAC * squareWidth;
+	boardWidth -= pieceDiameter;
+	double xLeft = (panelWidth - boardWidth) / 2 + MARGIN;
+	double yTop = (panelHeight - boardWidth) / 2 + MARGIN;
+
+	g2.setColor(Color.BLACK);
+	for (int i = 0; i < size; i++) {
+	    double offset = i * squareWidth;
+	    g2.draw(new Line2D.Double(xLeft, yTop + offset, 
+				      xLeft + gridWidth, yTop + offset));
+	    g2.draw(new Line2D.Double(xLeft + offset, yTop,
+				      xLeft + offset, yTop + gridWidth));
+	}
+	
+	for (int row = 0; row < size; row++) 
+	    for (int col = 0; col < size; col++) {
+		int piece = state.getPiece(row, col);
+		if (piece != OmokState.NONE) {
+		    Color c = (piece == OmokState.BLACK) ? Color.BLACK : Color.WHITE;
+		    g2.setColor(c);
+		    double xCenter = xLeft + col * squareWidth;
+		    double yCenter = yTop + row * squareWidth;
+		    Ellipse2D.Double circle
+			= new Ellipse2D.Double(xCenter - pieceDiameter / 2,
+					       yCenter - pieceDiameter / 2,
+					       pieceDiameter,
+					       pieceDiameter);
+		    g2.fill(circle);
+		    g2.setColor(Color.black);
+		    g2.draw(circle);
+		}
+	    }
+    }
+    
+    
+}
+
+
+
+
